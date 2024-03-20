e6f307b939f64e8887e461432c8a27249155dc06
levitesuo <leevi.suotula@gmail.com>
2024-03-20 14:45:26 +0200

    Not working, but working on it

--- src/a_star.py
+++ src/a_star.py
@@ -3,10 +3,11 @@ import heapq
 
 
 class Node:
-    def __init__(self, x, y, grid):
+    def __init__(self, x, y, grid, height_mapping_function):
         self._pos = (x, y)
         self.edges = [[0 for _ in range(3)] for _ in range(3)]
         self._grid = grid
+        self._height_mapping_function = height_mapping_function
 
         self.parent = (0, 0)
         self.f = float('inf')
@@ -19,28 +20,24 @@ class Node:
         y = self._pos[1]
         size = len(self._grid)
         for i in range(-1, 2):
-            if 0 <= x + i < size:
-                for j in range(-1, 2):
-                    if 0 <= y + j < size and not (i == 0 and j == 0):
-                        self.edges[i][j] = self._speed_calc(x+i, y+j)
-
-    def _speed_calc(self, x, y):
-        return height_mapping(self._pos[0], self._pos[1], x, y, self._grid)
-
-
-def height_mapping(x_pos, y_pos, x_goal, y_goal, grid):
-    return heigt_mapping_function(grid[x_goal][y_goal] - grid[x_pos][y_pos])
+            for j in range(-1, 2):
+                if 0 <= y + j < size and 0 <= x + i < size and not (i == 0 and j == 0):
+                    self.edges[i+1][j+1] = self._height_mapping_function(
+                        self._grid[x+i][y+j]-self._grid[x][y])
+                else:
+                    self.edges[i+1][j+1] = float('inf')
+        print(self.edges)
 
 
-def heigt_mapping_function(height_diff):
+def height_mapping_function(height_diff):
     a = 10
-    if height_diff < 0:
-        return abs(height_diff) + 1
-    return (height_diff * a) ** 2 + 1
+    if height_diff < -0.1:
+        return height_diff * -5 + 0.5 + 1
+    return (height_diff * a) * 20 + 3 + 1
 
 
 def get_node_grid(grid):
-    return [[Node(i, j, grid) for j in range(len(grid))] for i in range(len(grid))]
+    return [[Node(i, j, grid, height_mapping_function) for j in range(len(grid))] for i in range(len(grid))]
 
 
 def if_in_grid(x, y, grid):
@@ -51,15 +48,14 @@ def if_in_grid(x, y, grid):
 
 def h_function(x_pos, y_pos, x_goal, y_goal, grid):
     length = math.sqrt((x_pos-x_goal) ** 2 + (y_pos-y_goal) ** 2)
-    height_diff = grid[x_goal][y_goal] - grid[x_pos][y_pos]
-    return heigt_mapping_function(height_diff / length) * length
+    return length
 
 
 def trace_path(cell_details, dest):
     print("The Path is ")
     path = []
-    row = dest[1]
-    col = dest[0]
+    row = dest[0]
+    col = dest[1]
 
     # Trace the path from destination to source using parent cells
     while not (cell_details[row][col].parent[0] == row and cell_details[row][col].parent[1] == col):
@@ -68,9 +64,9 @@ def trace_path(cell_details, dest):
         temp_col = cell_details[row][col].parent[1]
         row = temp_row
         col = temp_col
-        if len(path) > 100000:
-            print("The lengt of the path is over 100000. Is this intended?")
-            return
+        if (row, col) in path:
+            print("ERROR: LOOP IN PATH")
+            return []
 
     # Add the source cell to the path
     path.append((row, col))
@@ -103,19 +99,21 @@ def a_star(start, goal, grid):
     heapq.heappush(open_list, (0, i, j))
 
     found_dest = False
-
+    with open('log.txt', 'w') as file:
+        file.write("")
     while len(open_list) > 0:
         p = heapq.heappop(open_list)
 
         i = p[1]
         j = p[2]
+        visited_cells[i][j] = True
 
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0),
                       (1, 1), (1, -1), (-1, 1), (-1, -1)]
         for dir in directions:
             new_i = i + dir[0]
             new_j = j + dir[1]
-            visited_cells[i][j] = True
+
             if if_in_grid(new_i, new_j, grid) and not visited_cells[new_i][new_j]:
                 if (new_i, new_j) == goal:
                     nodes[new_i][new_j].parent = (i, j)
@@ -126,12 +124,12 @@ def a_star(start, goal, grid):
                         nodes[i][j].edges[dir[0]+1][dir[1]+1]
                     new_h = h_function(new_i, new_j, goal[0], goal[1], grid)
                     new_f = new_g + new_h
-
-                    print(f"node: ({i}, {j}) new_g: {new_g}")
                     if nodes[new_i][new_j].f == float('inf') or nodes[new_i][new_j].f > new_f:
                         heapq.heappush(open_list, (new_f, new_i, new_j))
+                        with open('log.txt', 'a') as file:
+                            file.write(
+                                f"node: ({i}, {j})\tnewNode: ({new_i}, {new_j})\tnew_g: {new_g} \tnew_h: {new_h}\n")
                         nodes[new_i][new_j].f = new_f
                         nodes[new_i][new_j].g = new_g
                         nodes[new_i][new_j].h = new_h
                         nodes[new_i][new_j].parent = (i, j)
-                        print("KK")
---
+++ src/a_star2.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(sefl):
+        self._parent = (0, 0)
+        self._edges = []
+        sefl.
